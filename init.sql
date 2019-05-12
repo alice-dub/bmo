f3d7e91d8c5a8da99b05aa25efe51dd73b5836e6
Fabrice <fabrice.vaillant@centraliens.net>
2019-05-12 15:24:16 +0200

    [init.sql] Adds a file to give a rough db schema

---
+++ init.sql
@@ -0,0 +1,30 @@
+-- table
+CREATE table list_pdf (
+        id SERIAL PRIMARY KEY,
+        url text,
+        content text
+);
+
+-- user for the service/script
+create user bmo with password 'qsdfghjkl';
+GRANT CONNECT ON DATABASE bmo TO bmo;
+GRANT USAGE ON SCHEMA public TO bmo;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO bmo;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO bmo;
+
+-- extension to remove accent
+CREATE EXTENSION unaccent;
+-- search config to add accent removal
+CREATE TEXT SEARCH CONFIGURATION fr ( COPY = french );
+ALTER TEXT SEARCH CONFIGURATION fr ALTER MAPPING FOR hword, hword_part, word WITH unaccent, french_stem;
+
+-- the view
+CREATE MATERIALIZED VIEW search_index AS 
+SELECT id,
+       url,
+       to_tsvector(content) as document
+FROM list_pdf;
+CREATE INDEX idx_fts_search ON search_index USING gin(document);
+
+-- re gives right after creating the view
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO bmo;
