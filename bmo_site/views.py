d46387225eb4f86299142494a56e6c1a5fc55dc5
alice-dub <alice.duval99@gmail.com>
2019-05-12 14:52:23 +0200

    Add form

--- bmo_site/views.py
+++ bmo_site/views.py
@@ -1,8 +1,34 @@
 from django.http import HttpResponse
 from django.template import loader
 
+#def search(request):
+#    template = loader.get_template('search.html')
+#    print(request)
+#    requete = request.POST['search']
+#    #if requete:
+#    #	Resultat = ['Resultat1', 'Resultat2', 'Resultat3']
+#    context = {'Requete' : 'Resultat1', 'resultat': ''}  
+#    return HttpResponse(template.render(context, request))
+
 def search(request):
     template = loader.get_template('search.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    context={}
+    if request.method == 'GET':
+        query= request.GET.get('q')
 
+        submitbutton= request.GET.get('submit')
+
+        if query is not None:
+            #lookups= Q(title__icontains=query) | Q(content__icontains=query)
+
+            results= ["Resultat1", "Resultat2"]
+
+            context={'results': results,
+                     'submitbutton': submitbutton}
+
+            return HttpResponse(template.render(context, request))
+
+        else:
+            return HttpResponse(template.render(context, request))
+    else:
+        return HttpResponse(template.render(context, request))
