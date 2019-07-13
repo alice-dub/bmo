6c0b407cbc793a3c4e5aa6f038aee441cff94d03
Fabrice <fabrice.vaillant@centraliens.net>
2019-07-13 13:31:27 +0200

    Updates import script

--- test_import.py
+++ test_import.py
@@ -1,40 +1,65 @@
-#import psycopg2
 import re
 import psycopg2
 import requests
+import subprocess
 
 import bs4 as BeautifulSoup
 
-#div class="accordion-content-wrapper"
-traduction_mois = {'janvier':'01', 'février':'02', 'mars':'03', 'avril':'04', 'mai':'05', 'juin':'06', 'juillet':'07', 'août':'08', 'septembre':'09', 'octobre':'10', 'novembre':'11', 'décembre':'12'}
+
+conn = psycopg2.connect("dbname=bmo user=bmo password=qsdfghjkl")
+
+traduction_mois = {
+ 'janvier': '01',
+ 'février': '02',
+ 'mars': '03',
+ 'avril': '04',
+ 'mai': '05',
+ 'juin': '06',
+ 'juillet': '07',
+ 'août': '08',
+ 'septembre': '09',
+ 'octobre': '10',
+ 'novembre': '11',
+ 'décembre': '12'
+}
+
+
+def date_to_day(day):
+    if day == '1er':
+        return '01'
+    else:
+        return day.rjust(2, '0')
+
 
 res = requests.get("https://www.paris.fr/bmo")
 soup = BeautifulSoup.BeautifulSoup(res.text)
 bmo_paragraph = soup.findAll('div', {'class': 'accordion-content-wrapper'})
 for bmo_year in bmo_paragraph:
-	bmo_list = bmo_year.findAll('a')
+    bmo_list = bmo_year.findAll('a')
 
-	for bmo in bmo_list:
-		if bmo.text:
-			extract_date = bmo.text.split()[-3:]
-			if  bool(re.match("20\d\d", extract_date[2])):
-				date = '{}-{}-{}'.format(extract_date[2], traduction_mois[extract_date[1]], '01' if extract_date[0] == '1er' else extract_date[0].rjust(2, '0'))
-				print(bmo['href'], date)
+    for bmo in bmo_list:
+        if bmo.text:
+            extract_date = bmo.text.split()[-3:]
+            if bool(re.match(r"20\d\d", extract_date[2])):
+                date = '{}-{}-{}'.format(extract_date[2],
+                                         traduction_mois[extract_date[1]],
+                                         date_to_day(extract_date[0]))
+                url = bmo['href']
+                print(url, date)
+                curr = conn.cursor()
+                curr.execute("select id from list_pdf where url = %s",
+                             (url,))
+                if curr.fetchone():
+                    continue
+                r = requests.get(url, stream=True)
+                with open("/tmp/bmo.pdf", "wb") as handle:
+                    for chunk in r.iter_content(chunk_size=512):
+                        if chunk:  # filter out keep-alive new chunks
+                            handle.write(chunk)
 
-def old_version():
-	conn = psycopg2.connect("dbname=bmo user=bmo password=qsdfghjkl")
-	curr = conn.cursor()
-
-
-	f=open("tmp.txt", "r")
-	contents =f.read()
-	contents = contents.split()
-	files = {}
-	for url in contents:
-		#search = re.search('(.*\/)([^\.]+)(\.pdf$|$)', url)
-		search = re.search('(.*\/)(.*)($)', url)
-		text_name = '{}.txt'.format(search.group(2))
-		file = open(text_name, "r")
-		content = file.read()
-		curr.execute("INSERT INTO list_pdf (url, content) VALUES (%s, %s)", (url, content))
-	conn.commit()
+                proc = subprocess.run(["pdftotext", "/tmp/bmo.pdf", "-"],
+                                      capture_output=True)
+                curr.execute("""
+                    INSERT INTO list_pdf (url, content, pub_date)
+                    VALUES (%s, %s, %s)""", (url, proc.stdout, date))
+                conn.commit()
